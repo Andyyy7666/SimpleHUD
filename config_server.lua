0ae8c309c6a8c3f0a5e0917feaa4f5549bfcc15c
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-07-13 03:24:19 +0200

    Add files via upload

---
+++ config_server.lua
@@ -0,0 +1,6 @@
+-- For support join my discord: https://discord.gg/Z9Mxu72zZ6
+
+server_config = {
+    discordServerToken = "", -- discord bot token for permissions
+    guildId = "", -- discord guild id
+}
